e383c4fe169d90b936cc969dc1833c395c1f5a93
acerDebugman <460873250@qq.com>
2025-01-07 18:32:50 +0800

    fix [12,18]

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -1,14 +1,24 @@
+use std::ops::Deref;
+
 #[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
 
+impl Deref for Point {
+    type Target = Self;
+
+    fn deref(&self) -> &Self::Target {
+        &self
+    }
+}
+
 fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
     // TODO: Fix the compiler error by adding something to this match statement.
-    match optional_point {
+    match optional_point.as_deref() {
         Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_uppercase().to_string() + chars.as_str(),
     }
 }
 
@@ -15,7 +15,7 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter().map(|x| x.to_string()).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
